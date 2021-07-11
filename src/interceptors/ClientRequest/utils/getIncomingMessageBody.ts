309e7c4a41881a72e7fb9cbc82f80ad2b74b486a
fsubal <fsubal@users.noreply.github.com>
2021-07-11 15:20:34 +0900

    Use once() instead of stream.once

--- src/interceptors/ClientRequest/utils/getIncomingMessageBody.ts
+++ src/interceptors/ClientRequest/utils/getIncomingMessageBody.ts
@@ -1,20 +1,23 @@
+import { once } from 'events'
 import { IncomingMessage } from 'http'
 import { Stream } from 'stream'
 import * as zlib from 'zlib'
 
-export function getIncomingMessageBody(res: IncomingMessage): Promise<string> {
+export async function getIncomingMessageBody(res: IncomingMessage): Promise<string> {
   let responseBody = ''
   let stream: Stream = res
 
   if (res.headers['content-encoding'] === 'gzip') {
     stream = res.pipe(zlib.createGunzip())
   }
+  
+  stream.on('data', (chunk) => (responseBody += chunk));
 
-  return new Promise((resolve, reject) => {
-    stream.once('error', reject)
-    stream.on('data', (chunk) => (responseBody += chunk))
-    stream.once('end', () => {
-      resolve(responseBody)
-    })
-  })
+  once(res, 'error', { signal: controller.signal }).then(e => {
+    throw e;
+  });
+
+  return once(res, 'end', { signal: controller.signal }).then(() => {
+    return responseBody;
+  });
 }
